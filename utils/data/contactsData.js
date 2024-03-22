c6913630b0fb3faf78d938b50004fd64323a96df
deekayhub <deekay843424@gmail.com>
2024-03-23 02:07:08 +0530

    push portfolio

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'deekay843424@gmail.com',
+    phone: '+91 9631661811',
+    address: 'Sector 71, SAS Nagar mohali, Chandigarh - 160055 ',
+    github: 'https://github.com/',
+    facebook: 'https://www.facebook.com/',
+    linkedIn: 'https://www.linkedin.com/in/',
+    twitter: 'https://twitter.com/',
+    stackOverflow: 'https://stackoverflow.com/users/',
+    devUsername: ""
+}
