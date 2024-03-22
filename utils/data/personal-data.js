50f9288b17472bb2e6f0713014749d0f52af926e
deekayhub <53885762+deekayhub@users.noreply.github.com>
2024-03-23 02:26:20 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Deepak Kumar. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'deekay843424@gmail.com',
   phone: '+91 9631661811',
-  address: 'Sector 710, Chandigath - 10055 ',
+  address: 'Sector 71, Chandigath - 10055 ',
   github: 'https://github.com/',
   facebook: 'https://www.facebook.com/',
   linkedIn: 'https://www.linkedin.com/',
